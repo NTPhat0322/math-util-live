79f2d7569d2abe897510df2f23901f5c490a029a
NTPhat0322 <tienphat040322@gmail.com>
2024-10-16 10:31:25 +0700

    lan thu 2 dung github

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -18,6 +18,9 @@ public class MathUtilLive {
         long rs = MathUtil.getFactorial(5);
         System.out.println("5! = " + rs);
         
+        //thêm code sau lần đầu tiên dùng github - 10:30 AM October, 16th, 2024
+        System.out.println("6! = " + MathUtil.getFactorial(6));
+        System.out.println("7! = " + MathUtil.getFactorial(7));
     }
     
 }
